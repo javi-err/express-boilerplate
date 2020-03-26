86f3bd9460e338d12d29b17f569ce99faa66cb53
Javori Smart <jsmart1993@hotmail.com>
2020-03-26 12:02:24 -0400

    boilerplate changes finished

--- src/app.js
+++ src/app.js
@@ -6,8 +6,7 @@ const helmet = require('helmet')
 
 const app = express()
 
-const morganOption = (process.env.NODE_ENV === 'production')
-? 'tiny'
+const morganOption = (NODE_ENV === 'production')? 'tiny'
 : 'common';
 
 app.use(morgan(morganOption))
@@ -17,5 +16,16 @@ app.use(cors())
 app.get('/', (req, res) => {
   res.send('Hello, boilerplate!')
 })
+
+ app.use(function errorHandler(error, req, res, next) {
+     let response
+     if (NODE_ENV === 'production') {       
+       response = { error: { message: 'server error' } }
+     } else {
+       console.error(error)
+       response = { message: error.message, error }
+     }
+     res.status(500).json(response)
+   })
 
 module.exports = app
